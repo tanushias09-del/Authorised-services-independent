e6e4bc6c85588092bdef4827166e14e632ce8b4d
tanushias09-del <tanushias.09@gmail.com>
2026-09-11 11:06:12 +0530

    Restore brand query parameter logic with static Vite entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Header from './components/header';       
+import Header from './components/Header';       
 import Footer from './components/footer';
 import CookieBanner from './components/Cookiebanner';
 
